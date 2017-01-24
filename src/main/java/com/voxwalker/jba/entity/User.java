e229212e151d23cbbc964b58b5e7bd2bac826887
Jason <sysbender@gmail.com>
2017-01-24 17:16:03 -0500

    08 configure persistence

---
+++ src/main/java/com/voxwalker/jba/entity/User.java
@@ -0,0 +1,24 @@
+package com.voxwalker.jba.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
+}
