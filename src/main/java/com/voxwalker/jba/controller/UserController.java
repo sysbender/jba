869db61c37f5e593fbd8ff41d0e99ee20ba832bd
Jason <sysbender@gmail.com>
2017-01-24 23:26:18 -0500

    14 add user-detail, put all taglib in one jsp

--- src/main/java/com/voxwalker/jba/controller/UserController.java
+++ src/main/java/com/voxwalker/jba/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.voxwalker.jba.entity.User;
@@ -26,4 +27,10 @@ public class UserController {
 		return "users";
 	}
 
+	
+	@RequestMapping("/users/{id}")
+	public String detail(Model model, @PathVariable int id){
+		model.addAttribute("user", userService.findOne(id));
+		return "user-detail";
+	}
 }
--- src/main/webapp/WEB-INF/jsp/users.jsp
+++ src/main/webapp/WEB-INF/jsp/users.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ include file="../layout/taglib.jsp" %>
 
 <table class="table table-bordered table-hover table-striped">
 	<thead>
@@ -13,7 +13,8 @@
 		<c:forEach var="user" items="${users}">
 
 			<tr>
-				<td>${user.name }</td>
+				<td><a href="<spring:url value='/users/${user.id}.html'/>">${user.name }</a>
+				</td>
 			</tr>
 		</c:forEach>
 
