3581558eccf26c60128d7fa69a6eeecd888011ca
Jason <sysbender@gmail.com>
2017-01-26 11:37:23 -0500

    33 security : a user is not allowed to delete other's blog

--- src/main/java/com/voxwalker/jba/service/BlogService.java
+++ src/main/java/com/voxwalker/jba/service/BlogService.java
@@ -1,6 +1,8 @@
 package com.voxwalker.jba.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.voxwalker.jba.entity.Blog;
@@ -28,9 +30,15 @@ public class BlogService {
 	}
 
 
-	public void delete(int id) {
-	 blogRepository.delete(id);
+	@PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+	public void delete( @P("blog") Blog blog) {
+	 blogRepository.delete(blog);
 		
+	}
+
+
+	public Blog findOne(int id) {		
+		return blogRepository.findOne(id);
 	}
 
 }
